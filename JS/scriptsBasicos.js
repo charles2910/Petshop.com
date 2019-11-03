938c01ec3a31ac286c460c94e47744917ca5073c
Matheus Vieira <matheus.v4200@gmail.com>
2019-11-03 18:01:44 -0300

    Adicionando o objeto cliente e resolvendo bugs

--- JS/scriptsBasicos.js
+++ JS/scriptsBasicos.js
@@ -43,6 +43,78 @@ function navegarCompra(){
     AJAX_navegacao("../conteudos/compra.html");
 }
 
+class Cliente{
+    constructor(nome, email, celular,telefone,nascimento,cpf,senha,endereco,cartao){
+        this.nome = nome;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.nascimento = nascimento;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.endereco = endereco;
+        this.cartao = cartao;
+    }
+}
+
+class Endereco{
+    constructor(cep,rua,numero,bairro,complemento,estado,cidade){
+        this.cep = cep;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.complemento = complemento;
+        this.estado = estado;
+        this.cidade = cidade;
+    }
+}
+
+class Pagamento{
+    constructor(nome,numero,validade,cvv){
+        this.nome = nome;
+        this.numero = numero;
+        this.validade = validade;
+        this.cvv = cvv;
+    }
+}
+
+function cadastrarCliente(){
+    if(document.getElementById("senha1").value === document.getElementById("senha2").value){
+        let endereco = new Endereco(
+            document.getElementById("cep").value,
+            document.getElementById("rua").value,
+            document.getElementById("numero_casa").value,
+            document.getElementById("bairro").value,
+            document.getElementById("complemento").value,
+            document.getElementById("estado").value,
+            document.getElementById("cidade").value
+        )
+
+        let cartao = new Pagamento(
+            document.getElementById("nome_cartao").value,
+            document.getElementById("nmr_cartao").value,
+            document.getElementById("validade_cartao").value,
+            document.getElementById("cvv").value
+        )
+
+        let cliente = new Cliente(
+            document.getElementById("nome_cliente").value,
+            document.getElementById("email").value,
+            document.getElementById("ddd_cel").value +
+            document.getElementById("celular").value,
+            document.getElementById("ddd_tel").value +
+            document.getElementById("telefone").value,
+            document.getElementById("nascimento").value,
+            document.getElementById("cpf").value,
+            document.getElementById("cpf").senha,
+            endereco,
+            cartao
+        )
+    }else{
+        window.alert("As senhas devem ser identicas");
+    }
+}
+
 function AJAX_navegacao(arquivo,id,pagina_atual){
     
     let xhttp = new XMLHttpRequest();
@@ -63,7 +135,6 @@ function AJAX_navegacao(arquivo,id,pagina_atual){
             document.getElementById("janela_de_conteudo").innerHTML = this.responseText;
         }
     }
-    //console.log(xhttp);
     xhttp.open("GET",arquivo);
     xhttp.send();
 }
