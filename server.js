1e6bb1b5f0b350e7c532e945887a62799f54b756
Carlos Henrique Lima Melara <carloslimamelara@gmail.com>
2019-11-29 13:06:42 +0000

    Adicionado express com tabela de roteamento no arquivo server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/HTML/' + 'index_completo.html');
+   console.log("/ acessado");
+})
+
+app.get('/index.html', function (req, res) {
+   res.sendFile(__dirname + '/HTML/' + 'index_completo.html');
+   console.log("/index.html acessado");
+})
+
+var server = app.listen(8081, '10.142.0.2', function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
