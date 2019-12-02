a34daf157adda73f2d9ad7de5771b8720bb61cff
Matheus Vieira <matheus.v4200@gmail.com>
2019-12-02 17:31:57 -0300

    Os banners são carregados juntos com o banco de dados

--- server/couchDb.js
+++ server/couchDb.js
@@ -1,7 +1,29 @@
-const express = require('express');
+//necessário apenas para teste
+    const express = require('express');
+    const app = express();
+//------------------------------------
 const nano = require('nano')('http://admin:admin@localhost:5984');
 const fs = require('fs')
-const app = express();
+
+class Banner{
+    constructor(){
+        this.cachorros=[];
+        this.gatos=[];
+        this.roedores=[];
+        this.passaros=[];
+        this.peixes=[];
+        this.brinquedos=[];
+        this.saude=[];
+        this.alimentos=[];
+        this.higiene=[];
+        this.acessorios=[];
+        this.geral1=[];
+        this.geral2=[];
+        this.geral3=[];
+    }
+}
+
+const banners = new Banner();
 
 async function criarDb() {
   const db_list = await nano.db.list();
@@ -29,10 +51,10 @@ async function criarDb() {
         cartao: "",
         admin: true
     },"admin@admin.com")
-
   }
   if(db_list.indexOf("servicos")<0) await nano.db.create("servicos").catch();
   if(db_list.indexOf("agendamentos")<0) await nano.db.create("agendamentos").catch();
+  await carregarBanners();
 }
 
 async function carregarBancoDeDados(estoque){
@@ -51,13 +73,15 @@ async function carregarBancoDeDados(estoque){
 
 async function addUser(user){
     const usuarios = nano.use("usuarios");
-    usuarios.get(user.email).then((headers)=>{
-        return false
+    let retorno;
+    await usuarios.get(user.email).then((headers)=>{
+        retorno = false
     }).catch(async ()=>{
         await usuarios.insert(user,user.email);
-        return true
+        retorno = true
     });
-}
+    return retorno;
+}   
 
 async function findUser(email){
     const usuarios = nano.use("usuarios");
@@ -74,12 +98,14 @@ async function findUser(email){
 
 async function addProduto(produto){
     const produtos = nano.use("produtos");
-    produtos.get(produto.codigo).then((headers)=>{
-        return false
+    let retorno;
+    await produtos.get(produto.codigo).then((headers)=>{
+        retorno =  false
     }).catch(async ()=>{
         await produtos.insert(produto,produto.codigo);
-        return true
+        retorno = true
     });
+    return retorno;
 }
 
 async function findProduto(codigo){
@@ -108,42 +134,83 @@ async function addAgendamento(agendamento){
 
 async function findAgendamento(data){
     const agendamentos = nano.use("agendamentos");
-    agendamentos.get(data).then((headers)=>{
-        return headers
+    let retorno;
+    await agendamentos.get(data).then((headers)=>{
+        retorno = headers
     }).catch(()=>{
-        return false
+        retorno = false
     });
+    return retorno;
 }
 
 async function addServico(servico){
     const servicos = nano.use("servicos");
-    servicos.get(servico.id).then((headers)=>{
-        return false
+    let retorno;
+    await servicos.get(servico.id).then((headers)=>{
+        retorno = false
     }).catch(async ()=>{
         await servicos.insert(servico,servico.id);
-        return true
+        retorno = true
     });
+    return retorno;
 }
 
 async function getServicos(){
-    let produto = await findProduto("a0cb");
-    console.log(produto);
-    const servicos = nano.use("estoque");
-    servicos.list().then((body) => {
+    const servicos = nano.use("servicos");
+    let todosServicos = [];
+    let body = await servicos.list();
+    for(const id of body.rows){
+        let servico = await servicos.get(id.id);
+        todosServicos.push(servico);
+    }
+    return todosServicos;
+}
+
+async function carregarBanners(){
+    return new Promise( async(resolve)=> {
+        const estoque = nano.use("estoque");
+        const promocoes = []
+        body = await estoque.view('view', 'view', {
+            'key': [1,true]
+        })
         body.rows.forEach((doc) => {
-            //console.log(doc.doc);
+            promocoes.push(doc.value);
         });
+        promocoes.sort(compare);
+        promocoes.forEach((produto)=>{
+            const categoria = produto.categoria.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            const departamento = produto.departamento.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            if(banners[departamento].length < 12) banners[departamento].push(produto);
+            if(banners[categoria].length < 12) banners[categoria].push(produto);
+        });
+        promocoes.sort(compare1);
+        banners.geral2 = promocoes.slice(0,10);
+        promocoes.sort(compare2);
+        let i=0;
+        while(banners.geral3.length < 10){
+            if(promocoes[i].qtdEstoque !==0 && i<= promocoes.length){
+                banners.geral3.push(promocoes[i]);
+            }
+            i++;
+        }
+        resolve(true);
     });
 }
 
-criarDb();
+function compare(a,b){//Ordena pela % do precao
+    return b.precoPromocional-a.precoPromocional;
+}
 
-app.get('/',(req,res)=>{
-    res.send('Working...');
-})
+function compare1(a,b){//ordena pelo desconto real
+    return   ((b.preco) - (b.preco) *((100-b.precoPromocional)/100)) - (a.preco - (a.preco)*((100-a.precoPromocional)/100));
+}
 
-app.listen(3000,()=>{
-    console.log("Running...");
-})
-
-getServicos();
+function compare2(a,b){//Ordena pela quantidade em estoque
+    return a.qtdEstoque - b.qtdEstoque;
+}
+//Apenas para teste, parte a ser removida
+    criarDb();
+    app.listen(3000,()=>{
+        console.log("Running...");
+    })
+//---------------------------------------
