7972b3bc818156030be70188ee1cf61525b6fb1c
Carlos Henrique Lima Melara <carloslimamelara@gmail.com>
2019-12-04 18:45:19 -0300

    Conclusão de pedido feita

--- JS/carrinho.js
+++ JS/carrinho.js
@@ -126,9 +126,10 @@ function finalizarCompra(){
     }else if(carrinho.numProd <1){
         alert("Adicione algo no carrinho");
     }else{
+        logged = jsonToUser(await AJAX_geral(`http://trabweb.ddns.net:8082/api/usuarios/${logged.email}`));
         logged.pedidos.push(new Pedido(carrinho.produtos,"Pendente",carrinho.valorTotal,carrinho.numProd));
-        attDbCliente(logged);
-        carrinho.produtos = [];
+        logged.carrinho = new Carrinho();
+        await AJAX_geralPUT(`http://trabweb.ddns.net:8082/api/usuarios/${logged.email}`, logged);
         attCarrinho();
         carregarCarrinho();
         alert("Compra concluída com sucesso!");
