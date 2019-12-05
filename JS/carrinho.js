662fbe5b557318d5adbc66c6b9528401d3352b85
Carlos Henrique Lima Melara <carloslimamelara@gmail.com>
2019-12-05 19:06:50 -0300

    Checa se o usuário possui um carrinho ativo e o carrega

--- JS/carrinho.js
+++ JS/carrinho.js
@@ -1,10 +1,13 @@
 let carrinho;
-if (logged) {
-    if (logged.carrinho && logged.carrinho.numProd) {
-        carrinho = logged.carrinho;
+inicializaCarrinho();
+function inicializaCarrinho(){
+    if (logged) {
+        if (logged.carrinho && logged.carrinho.numProd) {
+            carrinho = logged.carrinho;
+        }
+    } else if(!carrinho) {
+        carrinho = new Carrinho();
     }
-} else {
-    carrinho = new Carrinho();
 }
 
 async function attCarrinho() {
@@ -81,6 +84,7 @@ function removerProduto(id) {
 }
 
 function carregarCarrinho() {
+    inicializaCarrinho();
     let txt = '<div id="conteudo_carrinho"><h1>Meu carrinho</h1><hr><table>  <tr><th>Produto</th><th>Nome</th><th>Quantidade</th><th>Preço</th><th>Remover</th></tr>';
 
     if (carrinho.numProd !== 0)
